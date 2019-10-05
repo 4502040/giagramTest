55acc0f1336981b87b65a30dbcb18e30e69a0b0c
Nurbol <4502040@gmail.com>
2019-10-05 22:39:24 +0600

    chart

--- GrpahicsTest/Form1.Designer.cs
+++ GrpahicsTest/Form1.Designer.cs
@@ -28,9 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.XYDiagramPane xyDiagramPane1 = new DevExpress.XtraCharts.XYDiagramPane();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView1 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView2 = new DevExpress.XtraCharts.LineSeriesView();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.pbox = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pbox2 = new System.Windows.Forms.PictureBox();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagramPane1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -46,19 +67,82 @@
             // panel1
             // 
             this.panel1.AutoSize = true;
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(931, 453);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(243, 489);
+            this.panel1.Size = new System.Drawing.Size(648, 489);
             this.panel1.TabIndex = 1;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // panel2
             // 
             this.panel2.AutoSize = true;
-            this.panel2.Location = new System.Drawing.Point(420, 48);
+            this.panel2.Location = new System.Drawing.Point(23, 10);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(146, 153);
+            this.panel2.Size = new System.Drawing.Size(247, 215);
             this.panel2.TabIndex = 2;
+            // 
+            // pbox
+            // 
+            this.pbox.Location = new System.Drawing.Point(24, 22);
+            this.pbox.Name = "pbox";
+            this.pbox.Size = new System.Drawing.Size(276, 233);
+            this.pbox.TabIndex = 3;
+            this.pbox.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1054, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Report";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1126, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pbox2
+            // 
+            this.pbox2.Location = new System.Drawing.Point(348, 22);
+            this.pbox2.Name = "pbox2";
+            this.pbox2.Size = new System.Drawing.Size(235, 233);
+            this.pbox2.TabIndex = 6;
+            this.pbox2.TabStop = false;
+            // 
+            // chartControl1
+            // 
+            xyDiagram1.AxisX.GridLines.Visible = true;
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1;0";
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1;0";
+            xyDiagram1.PaneLayoutDirection = DevExpress.XtraCharts.PaneLayoutDirection.Horizontal;
+            xyDiagramPane1.Name = "Pane 1";
+            xyDiagramPane1.PaneID = 0;
+            xyDiagram1.Panes.AddRange(new DevExpress.XtraCharts.XYDiagramPane[] {
+            xyDiagramPane1});
+            xyDiagram1.Rotated = true;
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(35, 12);
+            this.chartControl1.Name = "chartControl1";
+            series1.Name = "Series 1";
+            series1.View = lineSeriesView1;
+            series2.Name = "Series 2";
+            lineSeriesView2.PaneName = "Pane 1";
+            series2.View = lineSeriesView2;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1,
+        series2};
+            this.chartControl1.Size = new System.Drawing.Size(997, 479);
+            this.chartControl1.TabIndex = 7;
             // 
             // Form1
             // 
@@ -66,12 +150,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1272, 570);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.chartControl1);
+            this.Controls.Add(this.pbox2);
+            this.Controls.Add(this.pbox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagramPane1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +182,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pbox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pbox2;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
     }
 }
 
--- GrpahicsTest/Form1.cs
+++ GrpahicsTest/Form1.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using FastReport.MSChart;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+using FastReport;
+using DevExpress.XtraCharts;
 
 namespace GrpahicsTest
 {
@@ -19,6 +29,10 @@ namespace GrpahicsTest
 
         private void button1_Click(object sender, EventArgs e)
         {
+            DrawOnPBox();
+
+            return;
+
             List<DiModel> diModel = new List<DiModel>();
 
             Form frm = Application.OpenForms[0];
@@ -134,6 +148,350 @@ namespace GrpahicsTest
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void DrawOnPBox()
+        {
+            List<DiModel> diModel = new List<DiModel>();
+
+            Form frm = Application.OpenForms[0];
+
+
+            int i = 1;
+
+            Random rd = new Random();
+
+            for (DateTime dt = new DateTime(2019, 10, 1); dt <= new DateTime(2019, 10, 2); dt = dt.AddMinutes(5))
+            {
+                diModel.Add(new DiModel()
+                {
+                    Id = i,
+                    Date = dt.ToString("H:m dd.MM.yyyy"),
+                    Dt = dt,
+                    Davlenie = rd.Next(5000, 6000),
+                    Deepth = 100 + i * 5,
+                    Moment = rd.Next(90),
+                    Nagruzka = rd.Next(245),
+                    Oborot = rd.Next(100),
+                    Podacha = rd.Next(100),
+                    Speed = rd.Next(20),
+                    VesDown = rd.Next(400),
+                    VesUp = rd.Next(300),
+                });
+
+
+
+
+                //panel1.Controls.Add(lbl);
+
+                i++;
+            }
+
+            frm.Size = new Size() { Width = 1200, Height = 1200 };
+
+            pbox.Size = new Size() { Width = 150, Height = 1200 };
+            pbox.Top = 5;
+            pbox.Left = 10;
+            
+
+            i = 1;
+
+            
+
+            //panel1.Refresh();
+            //panel2.Refresh();
+
+            int gX1 = 0; int gY1 = 0;
+            int gX2 = 0; int gY2 = 0;
+
+            Graphics g = pbox.CreateGraphics();
+            
+            
+
+            g.Clear(Color.White);
+            
+
+            SolidBrush sbr = new SolidBrush(Color.Black);
+
+            FontFamily fam = new FontFamily("Tahoma");
+            Font font = new System.Drawing.Font(fam, 8, FontStyle.Regular);
+
+            
+
+            foreach (DiModel di in diModel)
+            {
+
+                int y = i * 13;
+
+                DiModel next = diModel.Where(d => d.Dt > di.Dt).FirstOrDefault();
+
+                g.DrawString(i + " - " + di.Date, font, sbr, new Point(10, y));
+
+                                
+                //gY1 = next.VesDown;
+
+                i++;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<DiModel> diModel = new List<DiModel>();
+            
+            int i = 1;
+
+            Random rd = new Random();
+
+            for (DateTime dt = new DateTime(2019, 10, 1); dt <= new DateTime(2019, 10, 2); dt = dt.AddMinutes(5))
+            {
+                diModel.Add(new DiModel()
+                {
+                    Id = i,
+                    Date = dt.ToString("H:m dd.MM.yyyy"),
+                    Dt = dt,
+                    Davlenie = rd.Next(5000, 6000),
+                    Deepth = 100 + i * 5,
+                    Moment = rd.Next(90),
+                    Nagruzka = rd.Next(245),
+                    Oborot = rd.Next(100),
+                    Podacha = rd.Next(100),
+                    Speed = rd.Next(20),
+                    VesDown = rd.Next(400),
+                    VesUp = rd.Next(300),
+                });
+
+
+              
+
+                i++;
+            }
+
+            
+
+
+            DevExpress.XtraCharts.Series s1 = chartControl1.Series[0];
+            DevExpress.XtraCharts.Series s2 = chartControl1.Series[1];
+            s1.Points.Clear();
+            s2.Points.Clear();
+
+            
+
+            foreach (DiModel di in diModel) {
+                s1.Points.AddPoint(di.Dt, di.VesUp);
+                //s1.Points.Add(new DevExpress.XtraCharts.SeriesPoint() { Argument = di.Date, Values = new [] {Convert.ToDouble(di.Deepth)} });
+                s2.Points.AddPoint(di.Dt, di.VesDown);
+            }
+
+            XYDiagram diagram = (XYDiagram)chartControl1.Diagram;
+
+            // Define the date-time measurement unit, to which the beginning of
+            // a diagram's gridlines and labels should be aligned.
+            diagram.AxisX.DateTimeScaleMode = DateTimeScaleMode.Manual;
+            //diagram.AxisX.DateTimeGridAlignment = DateTimeMeasurementUnit.Minute;
+
+            // Define the detail level for date-time values.
+            diagram.AxisX.DateTimeMeasureUnit = DateTimeMeasurementUnit.Minute;
+            diagram.AxisX.DateTimeScaleOptions.ScaleMode = ScaleMode.Continuous;
+
+            // Define the custom date-time format (name of a month) for the axis labels.
+            diagram.AxisX.DateTimeOptions.Format = DateTimeFormat.Custom;
+            diagram.AxisX.DateTimeOptions.FormatString = "dd/MM/yyyy\nHH:mm:ss";
+
+            chartControl1.Height = this.Height;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Image img = DrawText("Test", new System.Drawing.Font(new FontFamily("Tahoma"), 18, FontStyle.Regular), Color.Red, Color.Transparent);
+            List<DiModel> diModel = new List<DiModel>();
+            Random rd = new Random();
+            int i = 1;
+            for (DateTime dt = new DateTime(2019, 10, 1); dt <= new DateTime(2019, 10, 2); dt = dt.AddMinutes(5))
+            {
+                diModel.Add(new DiModel()
+                {
+                    Id = i,
+                    Date = dt.ToString("H:m dd.MM.yyyy"),
+                    Dt = dt,
+                    Davlenie = rd.Next(5000, 6000),
+                    Deepth = 100 + i * 5,
+                    Moment = rd.Next(90),
+                    Nagruzka = rd.Next(245),
+                    Oborot = rd.Next(100),
+                    Podacha = rd.Next(100),
+                    Speed = rd.Next(20),
+                    VesDown = rd.Next(400),
+                    VesUp = rd.Next(300),
+                });
+
+
+                //panel1.Controls.Add(lbl);
+
+                i++;
+            }
+                        
+
+            pbox.Image = DrawDateTimes(diModel);
+
+            pbox2.Image = DrawLines(diModel);
+        }
+
+        private Image DrawText(String text, Font font, Color textColor, Color backColor)
+        {
+            //first, create a dummy bitmap just to get a graphics object
+            Image img = new Bitmap(1, 1);
+            Graphics drawing = Graphics.FromImage(img);
+
+            //measure the string to see how big the image needs to be
+            SizeF textSize = drawing.MeasureString(text, font);
+
+            //free up the dummy image and old graphics object
+            img.Dispose();
+            drawing.Dispose();
+
+            //create a new image of the right size
+            img = new Bitmap((int)textSize.Width, (int)textSize.Height);
+
+            drawing = Graphics.FromImage(img);
+
+            //paint the background
+            drawing.Clear(backColor);
+
+            //create a brush for the text
+            Brush textBrush = new SolidBrush(textColor);
+
+            drawing.DrawString(text, font, textBrush, 0, 0);
+
+            drawing.Save();
+
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            return img;
+
+        }
+
+        private Image DrawDateTimes(List<DiModel> diModel)
+        {      
+
+            System.Drawing.Font font = new System.Drawing.Font(new FontFamily("Tahoma"), 7, FontStyle.Regular);
+
+            Color backColor = Color.Transparent;
+            Color textColor = Color.Black;
+            string text = "Super";
+
+            //first, create a dummy bitmap just to get a graphics object
+            Image img = new Bitmap(1, 1);
+            Graphics drawing = Graphics.FromImage(img);
+
+            //measure the string to see how big the image needs to be
+            SizeF textSize = drawing.MeasureString(text, font);
+
+            //free up the dummy image and old graphics object
+            img.Dispose();
+            drawing.Dispose();
+
+            int pboxHeight = (int)diModel.Count * 13;
+
+            //create a new image of the right size
+            pbox.Size = new Size() { Width = 150, Height = pboxHeight };
+            pbox.Top = 5;
+            pbox.Left = 10;
+            img = new Bitmap((int)pbox.Width, pboxHeight);
+
+            drawing = Graphics.FromImage(img);
+
+            //paint the background
+            drawing.Clear(backColor);
+
+            //create a brush for the text
+            Brush textBrush = new SolidBrush(textColor);
+
+            int i = 1;
+
+            foreach (DiModel di in diModel) {
+                int y = i * 12;
+                //int count = (int)(di.Dt.TotalSeconds / tsp2.t.TotalSeconds);
+                drawing.DrawString(di.Date, font, textBrush, new Point(10, y));
+                i++;
+            }            
+
+            drawing.Save();
+
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            return img;
+
+        }
+
+        private Image DrawLines(List<DiModel> diModel)
+        {     
+
+            int pboxHeight = (int)diModel.Count * 13;
+            int pboxWidth = diModel.Max(d => d.VesDown) + 10;
+            //create a new image of the right size
+            pbox2.Size = new Size() { Width = pboxWidth , Height = pboxHeight };
+            pbox2.Top = 5;
+            pbox2.Left = 130;
+
+            Image img = new Bitmap((int)pbox2.Width, pboxHeight);
+
+            Graphics drawing = Graphics.FromImage(img);
+                      
+
+            int i = 1;
+            int gX1 = 0; int gY1 = 0;
+            int gX2 = 0; int gY2 = 0;
+
+            drawing.PageUnit = GraphicsUnit.Point;
+            drawing.PageScale = 0.7F;
+
+            foreach (DiModel di in diModel)
+            {
+                int y = i * 12;
+                DiModel next = diModel.Where(d => d.Dt > di.Dt).FirstOrDefault();
+
+                if (next == null)
+                {
+                    next = new DiModel();
+                    gX1 = di.VesDown; gY1 = 0;
+                    gX2 = 20; gY2 = di.VesDown;
+                }
+                else
+                {
+                    gX1 = di.VesDown; gY1 = i * 2;
+                    gX2 = next.VesDown; gY2 = i * 2+2;
+
+                    var p = new Pen(Color.Black, 0.5f);
+                    var point1 = new Point(gX1, gY1);
+                    var point2 = new Point(gX2, gY2);
+                    drawing.DrawLine(p, point1, point2);
+                }
+
+                i++;
+            }
+
+            drawing.Save();
+
+           
+            drawing.Dispose();
+
+            return img;
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //chartControl1.Width = this.Width;
+           // chartControl1.Height = this.Height;
         }
     }
 
